4ec76a4710ed3e18d8e133585dfc4ce1bbf0c1f6
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-02-15 20:03:20 +0200

    Added basic Express server setup in app.js with a welcome route

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("server running on localhost:3000")
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker API! by Kgaugelo.Dev')
+})
+
+app.listen(3000, () => {
+    console.log('Subscription Tarcker API is running on http://localhost:3000')
+})
+
+export default app;
