37db9889b197383f60eafb2bd9a6d98e31b0c9f1
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-09-03 19:58:43 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link, Element } from 'react-scroll';
 
 const Header = () => {
   return (
-    <header className="bg-white border-b border-gray-200 fixed top-0 left-0 w-full z-50">
+    <header className="bg-white border-b border-gray-200 fixed top-0 left-0 w-full cursor-pointer z-50">
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
             <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12 h-12">
